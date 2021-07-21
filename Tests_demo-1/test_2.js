d7685cb3683d38ac6a7a4cc4eb2a8f2b1841e275
Halyna-B <galya.berdik@gmail.com>
2021-07-21 18:44:17 +0300

    add all tests functions for elementary tasks in folder Test_demo-1

---
+++ Tests_demo-1/test_2.js
@@ -0,0 +1,129 @@
+function attachEnvelope(env1, env2) {}
+
+attachEnvelope();
+
+mocha.setup("bdd");
+
+
+
+//Tests
+
+//Correct working
+describe("Verify that function attachEnvelope is working correct", function () {
+  it("Function attachEnvelope with env1{a:521.7, b:179.4}, env2{c:521.7, d:179.4} return 0", function () {
+    assert.equal(
+      attachEnvelope({ a: 521.7, b: 179.4 }, { c: 521.7, d: 179.4 }),
+      0
+    );
+  });
+
+  it("Function attachEnvelope with env1{a:31.5, b:100.4}, env2{c:11.5, d:48.6} return 1", function () {
+    assert.equal(
+      attachEnvelope({ a: 31.5, b: 100.4 }, { c: 11.5, d: 48.6 }),
+      1
+    );
+  });
+
+  it("Function attachEnvelope with env1{a:331.7, b:241.4}, env2{c:950.5, d:408.6} return 2", function () {
+    assert.equal(
+      attachEnvelope({ a: 331.7, b: 241.4 }, { c: 950.5, d: 408.6 }),
+      2
+    );
+  });
+
+  it("Function attachEnvelope with env1{a:57.3, b:31.1}, env2{c:130.5, d:62.5} return 2", function () {
+    assert.equal(
+      attachEnvelope({ a: 57.3, b: 31.1 }, { c: 130.5, d: 62.5 }),
+      2
+    );
+  });
+
+  it("Function attachEnvelope with env1{a:35.4, b:179.6}, env2{c:71.3, d:179.5} return 0", function () {
+    assert.equal(
+      attachEnvelope({ a: 35.4, b: 179.6 }, { c: 71.3, d: 179.5 }),
+      0
+    );
+  });
+});
+
+//Return correct data type
+describe("Verify that function attachEnvelope return correct data type", function () {
+  it("Function attachEnvelope with env1{a:123, b:59}, env2{c:75, d:37} return type of data - number", function () {
+    assert.isNumber(
+      attachEnvelope({ a: 123, b: 59 }, { c: 75, d: 37 }),
+      "Return type of data number"
+    );
+  });
+
+  it("Function attachEnvelope with env1{a:343.9, b:387.1}, env2{c:2864.26, d:6831.2} return type of data - number", function () {
+    assert.isNumber(
+      attachEnvelope({ a: 343.9, b: 387.1 }, { c: 2864.26, d: 6831.2 }),
+      "Return type of data number"
+    );
+  });
+
+  it("Function attachEnvelope with env1{a:152.7, b:61.9}, env2{c:152.7, d:61.9} return type of data - number", function () {
+    assert.isNumber(
+      attachEnvelope({ a: 152.7, b: 61.9 }, { c: 152.7, d: 61.9 }),
+      "Return type of data number"
+    );
+  });
+});
+
+//Incorrect arguments
+describe("Verify that function attachEnvelope accepts incorrect arguments", function () {
+  it("Function attachEnvelope accepts object with argument 0 ", function () {
+    assert.equal(
+      chessBoard({ a: 0, b: 389.2 }, { c: 42, d: 81.2 }),
+      `{status:'failed', reason:'TypeError: incorrect numbers was accept, arguments of this function should be > 0'}`
+    );
+  });
+
+  it("Function attachEnvelope accepts object with arguments <= 0 ", function () {
+    assert.equal(
+      chessBoard({ a: -31.95, b: 389.2 }, { c: -42, d: 81.2 }),
+      `{status:'failed', reason:'TypeError: incorrect numbers was accept, arguments of objects should be > 0'}`
+    );
+  });
+
+  it("Function attachEnvelope accepts object with arguments strings ", function () {
+    assert.equal(
+      chessBoard({ a: "1795.87", b: 8423.8 }, { c: "c", d: 3961.9 }),
+      `{status:'failed', reason:'TypeError: incorrect data type was accept, arguments of objects should be equal number'}`
+    );
+  });
+
+  it("Function attachEnvelope accepts object with arguments > 1000000 ", function () {
+    assert.equal(
+      chessBoard({ a: 14832.39, b: 94217.96 }, { c: 2368916.92, d: 49282.92 }),
+      `{status:'failed', reason:'TypeError: incorrect numbers was accept, arguments of objects should be < 1000000'}`
+    );
+  });
+
+  it("Function attachEnvelope accepts more then two objects ", function () {
+    assert.equal(
+      chessBoard(
+        { a: 142.39, b: 917.96 },
+        { c: 2366.92, d: 492.92 },
+        { c: 36891.2, d: 2.92 }
+      ),
+      `{status:'failed', reason:'TypeError: incorrect number of objects was accept, function attachEnvelope should accepts two objects'}`
+    );
+  });
+
+  it("Function attachEnvelope accepts 1 objects ", function () {
+    assert.equal(
+      chessBoard({ a: 142.39, b: 917.96 }),
+      `{status:'failed', reason:'TypeError: incorrect number of objects was accept, function attachEnvelope should accepts two objects'}`
+    );
+  });
+
+  it("Function attachEnvelope doesn't accepts objects ", function () {
+    assert.equal(
+      chessBoard(),
+      `{status:'failed', reason:'TypeError: incorrect arguments was accept, function attachEnvelope should accepts two objects'}`
+    );
+  });
+});
+
+
---
+++ Tests_demo-1/test_3.js
@@ -0,0 +1,138 @@
+function areaOfTriangles(...triangles) { }
+
+areaOfTriangles();
+
+mocha.setup("bdd");
+
+
+
+
+//Tests
+
+//Correct working
+describe("Verify that function areaOfTriangles is working correct", function () {
+
+  it("Function areaOfTriangles return ['ABC', 'MKN', 'DFE']", function () {
+    assert.equal(
+      areaOfTriangles([
+        { vertices: "ABC", a: 10, b: 20, c: 22.36 },
+        { vertices: "DFE", d: 2.35, f: 1.79, e: 1.56 },
+        { vertices: "MKN", m: 7.97, k: 3.61, n: 7 }
+      ]),
+      ['ABC', 'MKN', 'DFE']
+    );
+  });
+
+  it("Function areaOfTriangles return ['DFE', 'ABC', 'MKN']", function () {
+    assert.equal(
+      areaOfTriangles([
+        { vertices: "ABC", a: 12, b: 25, c: 27.9 },
+        { vertices: "DFE", d: 45.89, f: 71.31, e: 100 },
+        { vertices: "MKN", m: 30.79, k: 23.03, n: 30.79 }
+      ]),
+      ['DFE', 'ABC', 'MKN']
+    );
+  });
+
+  it("Function areaOfTriangles return ['MKN', 'DFE', 'ABC']", function () {
+    assert.equal(
+      areaOfTriangles([
+        { vertices: "ABC", a: 53.1, b: 70.85, c: 25.7 },
+        { vertices: "DFE", d: 69.51, f: 37.84, e: 77.9 },
+        { vertices: "MKN", m: 51.9, k: 31.72, n: 80 }
+      ]),
+      ['MKN', 'DFE', 'ABC']
+    );
+  });
+
+});
+
+
+//Return correct data 
+describe("Verify that function areaOfTriangles return correct data", function () {
+
+    it("The function's return response ['ABC', 'MKN', 'DFE'] is an array", function () {
+        assert.isArray(
+          areaOfTriangles([
+          { vertices: "ABC", a: 10, b: 20, c: 22.36 },
+          { vertices: "DFE", d: 2.35, f: 1.79, e: 1.56 },
+          { vertices: "MKN", m: 7.97, k: 3.61, n: 7 }
+      ]), "Function areaOfTriangles return an array");
+  });
+
+    it("The function's return array ['ABC', 'MKN', 'DFE'] include string 'MKN'", function () {
+        assert.include(
+          areaOfTriangles([
+          { vertices: "ABC", a: 10, b: 20, c: 22.36 },
+          { vertices: "DFE", d: 2.35, f: 1.79, e: 1.56 },
+          { vertices: "MKN", m: 7.97, k: 3.61, n: 7 }
+      ]), 'MKN');
+  });
+
+    it("The function's return array ['ABC', 'FEO', 'MKN', 'DFE'] has length of 4", function () {
+        assert.lengthOf(
+          areaOfTriangles([
+          { vertices: "ABC", a: 10, b: 20, c: 22.36 },
+          { vertices: "DFE", d: 2.35, f: 1.79, e: 1.56 },
+          { vertices: "MKN", m: 7.97, k: 3.61, n: 7 },
+          { vertices: "FEO", f: 9.12, e: 23.03, o: 30.79 }
+  ]), 4);
+});
+
+})
+
+//Accepts objects are not triangles
+describe("Verify that accepts objects in a function areaOfTriangles are not triangles", function () {
+
+    it("Objects { vertices: 'ABC', a: 42.7, b: 21.85, c: 10 } in a function areaOfTriangles is not triangle ", function () {
+      assert.equal(
+        areaOfTriangles([
+          { vertices: "ABC", a: 42.7, b: 21.85, c: 10 },
+          { vertices: "DFE", d: 69.51, f: 37.84, e: 77.9 },
+          { vertices: "MKN", m: 51.9, k: 31.72, n: 80 }
+        ]), 
+        `{status:'failed', reason:TypeError: argument of this function should be an array of objects with triangles, 'ABC' is not triangle}`
+      );
+    });
+  
+    it("Object { vertices: 'DFE', d: 0.7, f: 31.84, e: 22.1 } in a function areaOfTriangles is not triangle ", function () {
+      assert.equal(
+        areaOfTriangles([
+          { vertices: "ABC", a: 12, b: 25, c: 27.9 },
+          { vertices: "DFE", d: 0.7, f: 31.84, e: 22.1 },
+          { vertices: "MKN", m: 4, k: 1.24, n: 3 }
+        ]), 
+        `{status:'failed', reason:TypeError: argument of this function should be an array of objects with triangles, 'DFE' is not triangle}`
+      );
+    });
+  
+  });
+
+//Accepts incorrect objects 
+describe("Verify that accepts objects in a function areaOfTriangles are not correct", function () {
+
+    it("Objects { vertices:'MKN', m: 7.97, k: '3.61', n: 7 } has incorrect value 'k' - string ", function () {
+      assert.equal(
+        areaOfTriangles([
+          { vertices: "ABC", a: 10, b: 20, c: 22.36 },
+          { vertices: "DFE", d: 2.35, f: 1.79, e: 1.56 },
+          { vertices: "MKN", m: 7.97, k: '3.61', n: 7 },
+          { vertices: "FEO", f: 9.12, e: 23.03, o: 30.79 }
+        ]),
+        `{status:'failed', reason:TypeError: the sides of the triangle must be numbers, 'k' should be equal a number}`
+      );
+    });
+  
+    it("Object { vertices: 'DFE', d: 2.35, f: 1.79, e: -1.56 } has incorrect value 'e' - negative number  ", function () {
+      assert.equal(
+        areaOfTriangles([
+            { vertices: "ABC", a: 10, b: 20, c: 22.36 },
+            { vertices: "DFE", d: 2.35, f: 1.79, e: -1.56 },
+            { vertices: "MKN", m: 7.97, k: 3.61, n: 7 }
+          ]),
+        `{status:'failed', reason:TypeError: the sides of the triangle must be positive numbers, 'e' should be equal a positive number}`
+      );
+    });  
+
+})
+
