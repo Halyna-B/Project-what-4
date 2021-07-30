24bbee10690da01faa9936593bbd8daac36eca70
Halyna-B <galya.berdik@gmail.com>
2021-07-30 15:17:26 +0300

    fixed task3 - test Done

--- Task_HW4-day4/task3.js
+++ Task_HW4-day4/task3.js
@@ -1,5 +1,5 @@
 function chronos(year, month, day) {
-  const daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Satarday"];
+  const daysOfWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday", "Satarday", "Sunday"];
   const daysInMonth = 30;
   const monthInYear = 12;
   const daysInYear = monthInYear * daysInMonth;
@@ -10,17 +10,26 @@ function chronos(year, month, day) {
 
   const countLeapYears = (inputYear) => {
     let count = 0;
-    for (let i = 0; i <= inputYear; i++) {
+    for (let i = 1; i <= inputYear; i+=1) {
       if (isleapYear(i)) {
         count += 1;
       }
     }
-    return count; //Count extra days for all LeapYears 
+    return count ; //Count extra days for all LeapYears 
   };
 
-  const allChronosDays = year * daysInYear + countLeapYears(year) + month * daysInMonth + day;
+  let allChronosDays = year * daysInYear  + month * daysInMonth + day + countLeapYears( month <= 2 ? year - 1 : year)
   
-  return daysOfWeek[allChronosDays % daysOfWeek.length];
+  return daysOfWeek[allChronosDays % 7];
 }
 
-console.log(chronos(1001, 8, 24));
+
+console.log(chronos(1, 1, 1))
+console.log(chronos(1, 8, 24))
+console.log(chronos(1, 8, 25))
+console.log(chronos(1001, 8, 23))
+console.log(chronos(1001, 8, 24))
+console.log(chronos(2020, 8, 24))
+console.log(chronos(3020, 8, 24))
+console.log(chronos(1000, 1, 20))
+console.log(chronos(1000, 2, 30))
