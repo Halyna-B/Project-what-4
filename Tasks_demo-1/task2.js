4777dd98f46c02a3ed42a596b5d8fcc5a26f50af
Halyna-B <galya.berdik@gmail.com>
2021-08-02 22:22:32 +0300

    added task2 for demo-1

--- Tasks_demo-1/task2.js
+++ Tasks_demo-1/task2.js
@@ -9,6 +9,37 @@
 */
 
 function attachEnvelope(env1, env2) {
+  const valuesEnv1 = Object.values(env1);
+  const valuesEnv2 = Object.values(env2);
 
-    
+  function getMin(arr) {
+    return Math.min(...arr);
+  }
+
+  function getMax(arr) {
+    return Math.max(...arr);
+  }
+
+  const minSideEnv1 = getMin(valuesEnv1);
+  const maxSideEnv1 = getMax(valuesEnv1);
+  const minSideEnv2 = getMin(valuesEnv2);
+  const maxSideEnv2 = getMax(valuesEnv2);
+
+  function isLonger(val1, val2) {
+    return val1 > val2 ? true : false;
+  }
+
+  if (
+    isLonger(maxSideEnv1, maxSideEnv2) &&
+    isLonger(minSideEnv1, minSideEnv2)
+  ) {
+    return 1;
+  } else if (
+    isLonger(maxSideEnv2, maxSideEnv1) &&
+    isLonger(minSideEnv2, minSideEnv1)
+  ) {
+    return 2;
+  } else {
+    return 0;
+  }
 }
