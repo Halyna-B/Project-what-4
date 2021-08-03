7075c0fb7831d17873f992cd7a0ed9a5459a9ba0
Halyna-B <galya.berdik@gmail.com>
2021-08-03 17:38:24 +0300

    added task 6

--- Tasks_demo-1/task6.js
+++ Tasks_demo-1/task6.js
@@ -6,4 +6,11 @@
 Выход: строка с рядом чисел
 */
 
-function numberSequence(numb) {}
+function numberSequence(n, m) {
+  let squareNum = Math.round(Math.sqrt(m));
+  const resultArr = [squareNum];
+  for (let i = 1; i < n; i++) {
+    resultArr.push((squareNum += 1));
+  }
+  return resultArr.join();
+}
