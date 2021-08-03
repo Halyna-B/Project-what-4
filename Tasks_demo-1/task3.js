3cd1a549eeab47da350b2e31de01d5ec41b41324
Halyna-B <galya.berdik@gmail.com>
2021-08-03 11:56:08 +0300

    added task3 and change test3.js

--- Tasks_demo-1/task3.js
+++ Tasks_demo-1/task3.js
@@ -17,4 +17,24 @@ c: 22.36
 }
 */
 
-function areaOfTriangles(...triangles) { }
+function areaOfTriangles(triangles) {
+    
+  function formHerona(a, b, c) {
+    const p = (a + b + c) / 2;
+    const answer = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    return Math.trunc(answer);
+  }
+
+  const areasObj = triangles.reduce((obj, el) => {
+    const side = el.vertices.toLowerCase().split("");
+    obj[el.vertices] = formHerona(el[side[0]], el[side[1]], el[side[2]]);
+    return obj;
+  }, {});
+
+  function sortedAreas(obj) {
+    const sortedArr = Object.entries(obj).sort((a, b) => b[1] - a[1]);
+    return sortedArr.map((el) => el[0]);
+  }
+
+  return sortedAreas(areasObj);
+}
--- Tests_demo-1/test_3.js
+++ Tests_demo-1/test_3.js
@@ -1,4 +1,4 @@
-function areaOfTriangles(...triangles) { }
+function areaOfTriangles(triangles) { }
 
 areaOfTriangles();
 
