e7853b864b9cc8bde1e5c843193e4f5dbda89e2e
Halyna-B <galya.berdik@gmail.com>
2021-08-05 21:50:40 +0300

    added task7

--- Tasks_demo-1/task7.js
+++ Tasks_demo-1/task7.js
@@ -6,21 +6,65 @@
 Выход: массив чисел
 */
 
-// function Fibonacci(context) {
-//   if (context.hasOwnProperty("length")) countNumb = context.length;
-//   const fibArr = [0, 1].slice(0, countNumb);
+function fibonacci(context) {
 
-//   while (fibArr.length < countNumb) {
-//     fibArr.push(fibArr.slice(-2).reduce((acc, el) => acc + el));
-//   }
+  try{
+    
+    if(typeof context !== 'object'){
+      throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument 'context' must be an object`}
+      }
 
-//   return fibArr;
-// }
+    if(!context.hasOwnProperty('min','max') && !context.hasOwnProperty('length')){
+      throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument 'context' must be an object with fields 'length' or 'min,max'`}
+    }
 
-function fibonacci(context){
-    if (context.hasOwnProperty("length"))
-    return  Array.from(context).reduce(
-       (acc, _, i) => acc.concat(i > 1 ? acc[i - 1] + acc[i - 2] : i),
-       []
-     );
-   }
+    const isValid = (el) => {
+      if(typeof el!== 'number'){
+        throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input arguments of object must be a number `}
+      }
+      if(!Number.isInteger(el)){
+        throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input arguments of object must be an integer number`}
+      }
+      if( el < 1 ){
+        throw {status:'failed', reason: `RangeError: incorrect arguments was accept, input arguments of object must be greater than 0`}
+      }
+    }
+
+      if(context.hasOwnProperty('length')){
+        lengthValid = isValid(context.length);
+      }
+
+      if(context.hasOwnProperty('min','max')){
+        minMaxValid = Object.values(context).forEach(el => isValid(el));
+        if(context.min > context.max){
+          throw {status:'failed', reason: `RangeError: incorrect arguments was accept, in object 'context' value 'max' must be greater than value 'min'`}
+        }
+      }
+
+    let fibArr = [];
+  
+    if(context.hasOwnProperty('length')){
+    countNumb = context.length;
+    fibArr = [0, 1].slice(0, countNumb)
+    
+    while(fibArr.length < countNumb) {
+    fibArr.push(fibArr.slice(-2).reduce((acc, el) => acc + el))
+    }
+   return fibArr
+    }
+  
+  if(context.hasOwnProperty('min','max')){
+   startNum = context.min;
+   maxNumb = context.max;
+  
+  for(let i = 1, sum = 1; sum <= maxNumb; sum += i, i = sum - i){
+    if(sum >= startNum)
+    fibArr.push(sum);
+  }
+  return fibArr
+  }
+  }catch(err){
+    return `${err.status}, ${err.reason}`
+  }
+}
+
