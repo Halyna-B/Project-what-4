a077ff99d65c18d2de8382cf1cf1ef43bf59761d
Halyna-B <galya.berdik@gmail.com>
2021-08-06 14:20:30 +0300

    change task1

--- Tasks_demo-1/task1.js
+++ Tasks_demo-1/task1.js
@@ -13,37 +13,28 @@
 function chessBoard(length, width, symb) {
   try {
 
-    if(typeof length !== 'number'){
-      throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument ${length} must be a number`}
-    }
-
-    if(typeof width !== 'number'){
-      throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument ${width} must be a number`}
-    }
-
-    if(!Number.isInteger(length)){
-      throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument ${length} must be an integer number`}
-    }
-
-    if(!Number.isInteger(width)){
-      throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument ${width} must be an integer number`}
-    }
-
-    if( length < 0 || length >20){
-      throw {status:'failed', reason: `RangeError: incorrect arguments was accept, input argument ${length} must be greater than 0 and less than 20 `}
-    }
-
-    if( width < 0 || width >20){
-      throw {status:'failed', reason: `RangeError: incorrect arguments was accept, input argument ${width} must be greater than 0 and less than 20 `}
-    }
-
-    if(typeof symb !== 'string'){
-      throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument ${symb} must be a string`}
-    }
-
-    if(symb.length !== 1){
-      throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument ${symb} must be a single symbol with length 1`}
-    }
+    [length, width].forEach(argument => {
+      if(typeof argument !== 'number'){
+        throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument ${argument} must be a number`}
+      }
+      if(!Number.isInteger(argument)){
+        throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument ${argument} must be an integer number`}
+      }
+      if( argument < 0){
+        throw {status:'failed', reason: `RangeError: incorrect arguments was accept, input argument ${argument} must be greater than 0`}
+      }
+      if( argument > 20){
+        throw {status:'failed', reason: `RangeError: incorrect arguments was accept, input argument ${argument} must be less than 20 `}
+      }
+     });
+  
+      if(typeof symb !== 'string'){
+        throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument ${symb} must be a string`}
+      }
+  
+      if(symb.length !== 1){
+        throw {status:'failed', reason: `TypeError: incorrect arguments was accept, input argument ${symb} must be a single symbol with length 1`}
+      }
 
     let board = "";
     for (let i = 0; i < length; i += 1) {
